3b5cdf2620c3c2b44ab85fe63234366e15f8e80b
Vishesh Gupta <136686473+VG-05@users.noreply.github.com>
2024-05-22 17:48:00 +0400

    Style Changes To Add View for Meals

--- Fitness.Models/Meal.cs
+++ Fitness.Models/Meal.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,9 @@ namespace Fitness.Models
         [Key]
         public int Id { get; set; }
         public int Api_Id { get; set; }
+        [DisplayName("Food")]
         public string? FoodName { get; set; } = string.Empty;
+		[DisplayName("Brand")]
 		public string? BrandName { get; set; } = string.Empty;
 		public double? Servings { get; set; }
         public double? ServingSizeAmount { get; set; }
@@ -23,7 +26,8 @@ namespace Fitness.Models
         public double? Protein { get; set; }
         public double? Fat { get; set; }
         [AllowedValues("Breakfast", "Lunch", "Snacks", "Dinner")]
-        public string? MealTime { get; set; } = string.Empty;         // "Breakfast", "Lunch", "Dinner", "Snacks"
+		[DisplayName("Meal Time")]
+		public string? MealTime { get; set; } = string.Empty;         // "Breakfast", "Lunch", "Dinner", "Snacks"
         public DateOnly Date { get; set; }
     }
 }
